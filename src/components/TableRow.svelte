ad8ff96031c27e089a302b8b543fdf3fc8ce5bf2
Bobby Moynihan <bobby@madhive.com>
2022-07-26 03:46:12 -0500

    wingsuite TableWrapper

---
+++ src/components/TableRow.svelte
@@ -0,0 +1,17 @@
+<script>
+    import TableCell from "./TableCell.svelte";
+    export let row = {};
+    export /**
+* @type {any[]}
+*/
+     let columns = [];
+</script>
+<tr>
+    {#if $$slots.default}
+        <slot {row} {columns} />
+    {:else if row}
+        {#each columns as column}
+            <TableCell {row} {column} />
+        {/each}
+    {/if}
+</tr>
